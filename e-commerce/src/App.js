5c6650114793f39fe132161d8f1bc8929a839e9f
Mahesh101999 <129728776+Mahesh101999@users.noreply.github.com>
2023-07-17 19:23:29 +0530

    initial commit

---
+++ e-commerce/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Navigation from './customer/components/navigation/Navigation';
+import HomePage from './customer/pages/homepage/HomePage';
+
+function App() {
+  return (
+    <div className="">
+      <Navigation/>
+     <div><HomePage/></div>
+    </div>
+  );
+}
+
+export default App;
---
+++ e-commerce/src/customer/pages/homepage/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainCarosel from '../../components/homeCausorual/MainCarosel'
+
+const HomePage = () => {
+  return (
+    <div>
+      <MainCarosel/>
+      <div>other sections</div>
+    </div>
+   
+  )
+}
+
+export default HomePage
